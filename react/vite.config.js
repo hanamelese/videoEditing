2802a4dd91e3e60913a92ed0b4a7d2e9c20dae35
hanamelese <hanamelese95@gmail.com>
2025-09-20 01:07:42 -0700

    deploy in vercel

--- react/vite.config.js
+++ react/vite.config.js
@@ -1,7 +1,28 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Add any modules Vite warns about here
+      external: ['react/jsx-runtime']
+    }
+  },
+  resolve: {
+    alias: {
+      // Helps Vercel/Node resolve react runtime correctly
+      'react/jsx-runtime': 'react/jsx-runtime.js'
+    }
+  }
 })
